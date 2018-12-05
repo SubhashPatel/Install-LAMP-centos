ea6358784bcc510d43521fd068e6c44ce18201d1
Subhash Vadadoriya <it.subhashpatel@gmail.com>
2018-12-05 19:59:25 +0530

    Update and rename Uninstall-LAMP-centos.sh to uninstall-lamp-centos.sh

---
+++ uninstall-lamp-centos.sh
@@ -0,0 +1,31 @@
+
+#!/bin/sh
+
+#######################################
+# Bash script to uninstall an LAMP stack in CentOS
+# Author: Subhash (serverkaka.com)
+
+# Check if running as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Uninstall APache
+systemctl stop httpd
+yum remove -y httpd
+
+# Uninstal PHP
+yum remove php php-mysql php-pdo php-gd php-mbstring -y
+
+# Uninstall MySql
+systemctl stop mysqld.service && yum remove -y mysql-community-server && rm -rf /var/lib/mysql && rm -rf /var/log/mysqld.log && rm -rf /etc/my.cnf
+
+# Uninstall PhpMyAdmin
+yum remove -y epel-release
+sudo remove -y install phpmyadmin
+
+# Clean File
+yum autoremove
+
+echo LAMP server uninstallation completed.
